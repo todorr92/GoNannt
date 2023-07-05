44ec53c90fa0c0aa9db3c587b10ea0137ff238ea
unknown <todorovicslobodan10@gmail.com>
2023-07-05 14:26:55 +0100

    added Navbar and Footer component with styles.css file

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../assets/styles.css";
+
+function Footer() {
+  return (
+    <>
+      <h2>This is footer content</h2>
+    </>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../assets/styles.css";
+
+function Navbar() {
+  return (
+    <>
+      <h2>This is a navbar content</h2>
+      <nav>
+        <ul>
+          <li>List 1</li>
+          <li>List 2</li>
+          <li>List 3</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
