7ae0eee85b73d48ae0f0334ff594fd1310de12c9
unknown <todorovicslobodan10@gmail.com>
2023-07-05 20:47:00 +0100

    added icons to the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import "../assets/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <>
-      <h2>This is footer content</h2>
+      <footer>
+        <FontAwesomeIcon icon={faFacebookSquare} style={{ color: "#ffffff" }} />
+        <FontAwesomeIcon icon={faTwitterSquare} style={{ color: "#ffffff" }} />
+        <FontAwesomeIcon
+          icon={faInstagramSquare}
+          style={{ color: "#ffffff" }}
+        />
+        <FontAwesomeIcon icon={faLinkedin} style={{ color: "#ffffff" }} />
+      </footer>
     </>
   );
 }
