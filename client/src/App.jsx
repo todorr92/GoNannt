5a5b857c4522006a7647aef41ba7285c5e97531e
unknown <todorovicslobodan10@gmail.com>
2023-07-06 15:46:29 +0100

    added routes to the app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
       <Footer />
+      <Routes>
+        <Route path="/" element={<Navbar />}></Route>
+      </Routes>
     </>
   );
 }
