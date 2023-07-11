162e5a75c5eeb789f6a67f9ea7ed2c4cf0b0b5ac
unknown <todorovicslobodan10@gmail.com>
2023-07-11 13:17:09 +0100

    added Link paths to the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,35 @@ import {
   faTwitterSquare,
 } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <>
       <footer>
         <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4 col-sm-12">
+              <div className="card" style={{ width: "18rem" }}>
+                <h2>For Parents</h2>
+                <div className="card-body">
+                  <Link to="/parents/FAQ's">FAQ's for Parents</Link> <br />
+                  <Link to="/sign-up">Signup as a Parent</Link> <br />
+                  <Link to="">Trust on GoNanny</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-12">
+              <div className="card" style={{ width: "18rem" }}>
+                <h2>For sitters</h2>
+                <div className="card-body">
+                  <Link to="/sitters/FAQ's">FAQ's for Sitters</Link> <br />
+                  <Link to="/jobs-board">Jobs Board</Link> <br />
+                  <Link to="/sign-up">Signup as a Sitter</Link>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="row">
             <div className="col-12">
               <FontAwesomeIcon
