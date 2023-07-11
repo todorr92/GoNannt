4e00dc23bc09f4d95074faa8756a11207d0236bc
unknown <todorovicslobodan10@gmail.com>
2023-07-11 13:01:53 +0100

    added some content to Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileLines,
+  faUsers,
+  faPeopleRoof,
+} from "@fortawesome/free-solid-svg-icons";
 function Header() {
   return (
     <>
@@ -21,36 +27,58 @@ function Header() {
         </div>
         <div className="container">
           <div className="row">
-            <h3>How it works</h3>
+            <h3 className="text-center">Here's how it works</h3>
             <div className="col-md-4 col-sm-12">
               <div className="card" style={{ width: "18rem" }}>
-                <img src="" className="card-img-top" alt="..." />
+                <FontAwesomeIcon
+                  icon={faFileLines}
+                  size="2xl"
+                  style={{ color: "#7fd1de" }}
+                />
                 <div className="card-body">
                   <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
+                    Posting your first job is easy, simply sign up, create your
+                    profile and you’re ready to tell us what you need. Add your
+                    location, a job description, any special requirements and a
+                    rate you’re willing to pay.
                   </p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-12">
               <div className="card" style={{ width: "18rem" }}>
-                <img src="" className="card-img-top" alt="..." />
+                <FontAwesomeIcon
+                  icon={faUsers}
+                  size="2xl"
+                  style={{ color: "#7fd1de" }}
+                />
                 <div className="card-body">
                   <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
+                    Once you’ve posted your job, we’ll smart-match sitters and
+                    share their profiles with you. You can send direct invites
+                    to these, and any other, sitters on SureSitter to apply for
+                    your job. Sitters will also be able to search and apply
+                    through the jobs board. Applications for your job will
+                    appear in your dashboard, you can review, accept and reject
+                    them.
                   </p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-12">
               <div className="card" style={{ width: "18rem" }}>
-                <img src="" className="card-img-top" alt="..." />
+                <FontAwesomeIcon
+                  icon={faPeopleRoof}
+                  size="2xl"
+                  style={{ color: "#7fd1de" }}
+                />
                 <div className="card-body">
                   <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
+                    As applications come in, you can review their profiles and
+                    start interviewing. This process is entirely up to you, most
+                    parents arrange a call and then meet sitters before making a
+                    decision. Once you’ve decided on a sitter for your job,
+                    simply agree the terms and pay, and enjoy the support.
                   </p>
                 </div>
               </div>
