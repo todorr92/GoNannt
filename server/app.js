954af1b085e0e6346602f6b7a2332c15148bf082
unknown <todorovicslobodan10@gmail.com>
2023-07-21 14:53:19 +0100

    added job controller with all of the methods and updated app.js file to use it

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+//IMPORTING JOBS ROUTES
+const jobRoutes = require("./routes/jobs");
+// EXPRESS APP
 const app = express();
 const Parent = require("./models/parent");
 
 require("dotenv").config();
 const uri = process.env.DB_URI;
 
+// MIDDLEWARE
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/jobs", jobRoutes);
+
+//CONNECT TO DB
 async function connect() {
   try {
     await mongoose.connect(uri);
---
+++ server/controllers/jobController.js
@@ -0,0 +1,103 @@
+const Job = require("../models/job");
+const mongoose = require("mongoose");
+
+// get all jobs
+const getJobs = async (req, res) => {
+  const jobs = await Job.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(jobs);
+};
+
+// get a single job
+const getJob = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such job" });
+  }
+
+  const job = await Job.findById(id);
+
+  if (!job) {
+    return res.status(404).json({ error: "No such job" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// create a new job
+const createJob = async (req, res) => {
+  const { title, description, address } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!description) {
+    emptyFields.push("description");
+  }
+  if (!address) {
+    emptyFields.push("address");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
+  // add to the database
+  try {
+    const job = await Job.create({ title, description, address });
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a job
+const deleteJob = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such job" });
+  }
+
+  const job = await Job.findOneAndDelete({ _id: id });
+
+  if (!job) {
+    return res.status(400).json({ error: "No such job" });
+  }
+
+  res.status(200).json(job);
+};
+
+// update a job
+const updateJob = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such job" });
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!job) {
+    return res.status(400).json({ error: "No such job" });
+  }
+
+  res.status(200).json(job);
+};
+
+module.exports = {
+  getJobs,
+  getJob,
+  createJob,
+  deleteJob,
+  updateJob,
+};
