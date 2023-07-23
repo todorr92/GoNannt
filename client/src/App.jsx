56ce4a05aa47f3b1575db97b27750e06932528c4
unknown <todorovicslobodan10@gmail.com>
2023-07-23 14:01:04 +0100

    deleted postJob component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,6 @@ import Login from "./pages/Login";
 
 // REACT IMPORTS
 import { Route, Routes } from "react-router-dom";
-import PostJob from "./pages/PostJob";
 
 function App() {
   return (
@@ -38,7 +37,6 @@ function App() {
         ></Route>
         <Route path="/sitters/FAQ's" element={<FAQSitters />}></Route>
         <Route path="/jobs-board" element={<JobsBoard />}></Route>
-        <Route path="/jobs-board/post-job" element={<PostJob />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
       </Routes>
