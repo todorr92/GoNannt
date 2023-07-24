4baa2c52223222ba9c8d6592badae6e9820cd7d0
unknown <todorovicslobodan10@gmail.com>
2023-07-24 18:28:41 +0100

    added some extra email and password validation using Validator package

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+// to encrypt the password
 const bcrypt = require("bcrypt");
+// to validate email and password
+const validator = require("validator");
 
 const userSchema = new Schema(
   {
@@ -32,6 +34,16 @@ const userSchema = new Schema(
 
 //static signup method
 userSchema.statics.signup = async function (name, email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
   // extra layer of check that email is unique
   const exists = await this.findOne({ email });
   if (exists) {
