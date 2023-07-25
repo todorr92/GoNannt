580b072f94b951d2e0c12f99a8ac251719dbae9b
unknown <todorovicslobodan10@gmail.com>
2023-07-25 12:59:32 +0100

    added useState to Signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
   return (
     <>
       <div className="container-fluid" id="join-us">
@@ -8,15 +17,33 @@ function Signup() {
           <form id="register">
             <h1 className="h3 mb-3">Please register</h1>
             <div className="form-floating">
-              <input type="text" className="form-control" id="name" />
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
               <label htmlFor="name">Full Name</label>
             </div>
             <div className="form-floating">
-              <input type="email" className="form-control" id="email" />
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
               <label htmlFor="email">Email</label>
             </div>
             <div className="form-floating">
-              <input type="password" className="form-control" id="password" />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
               <label htmlFor="password">Password</label>
             </div>
             <button className="btn btn-primary" type="submit">
