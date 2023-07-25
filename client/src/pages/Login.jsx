757f014b30c1378aafba0d8285c1085b64e44132
unknown <todorovicslobodan10@gmail.com>
2023-07-25 12:51:02 +0100

    removed additonal form from login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,28 +18,6 @@ function Login() {
               Login
             </button>
           </form>
-          <form id="register" className="d-none">
-            <h1 className="h3 mb-3">Please register</h1>
-            <div className="form-floating">
-              <input type="date" className="form-control" id="date-of-birth" />
-              <label htmlFor="date-of-birth">Date of Birth</label>
-            </div>
-            <div className="form-floating">
-              <input type="text" className="form-control" id="name" />
-              <label htmlFor="name">Full Name</label>
-            </div>
-            <div className="form-floating">
-              <input type="email" className="form-control" id="email" />
-              <label htmlFor="email">Email</label>
-            </div>
-            <div className="form-floating">
-              <input type="password" className="form-control" id="password" />
-              <label htmlFor="password">Password</label>
-            </div>
-            <button className="btn btn-primary" type="submit">
-              Register
-            </button>
-          </form>
         </div>
       </div>
     </>
