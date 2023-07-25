180d3725e2d919646acd89bf4403bbdc3bb676e5
unknown <todorovicslobodan10@gmail.com>
2023-07-25 11:30:25 +0100

    wrapped whole app in auth context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { JobsContextProvider } from "./context/JobsContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <JobsContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </JobsContextProvider>
+    <AuthContextProvider>
+      <JobsContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </JobsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
