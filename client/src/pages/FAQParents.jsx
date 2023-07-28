fc1783a896ecac5a5e2b742728c7f1b7c9cee532
unknown <todorovicslobodan10@gmail.com>
2023-07-28 19:38:49 +0100

    added content to FAQ parents page

--- client/src/pages/FAQParents.jsx
+++ client/src/pages/FAQParents.jsx
@@ -1,7 +1,84 @@
 import React from "react";
 
 const FAQParents = () => {
-  return <div>FAQParents</div>;
+  return (
+    <>
+      FAQParents
+      <div className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6 landing-text-container">
+              <h1 className="landing-header header-color">FAQ's for Parents</h1>
+              <p className="landing-paragraph text-color">
+                Everything you need to know as a parent on SureSitter.
+              </p>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <img src="src/assets/images/bottle.png" alt="Bottle" />
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <h3>What do we do?</h3>
+          <p>
+            SureSitter was founded by parents, for parents, to help make it easy
+            for you to find childcare that others in your community know, trust
+            and use. The first thing to say is that we’re not a babysitting or
+            nanny agency, but a marketplace, with the aim of giving all of us as
+            parents the ability to view, review and make choices about the
+            childcare options available to us. We are not a childcare agency. We
+            do not personally meet or vet the caregivers on our platform.
+            Instead we provide parents with the tools and connections to help
+            find the best caregivers that will work for you and your family.
+          </p>
+          <h3>How do I sign-up to SureSitter as a parent?</h3>
+          <p>
+            Signing up is really simple.  Click on this link. You will then
+            receive an email asking you to confirm your account. You must
+            confirm your account before your profile will be live onSureSitter.
+          </p>
+          <h3>How much does it cost?</h3>
+          <p>
+            It’s FREE to post a job and review childminder profiles. You only
+            pay when you find suitable carers that you want to connect with. We
+            offer a number of options to suit all families. This includes a
+            one-off, no-commitment option of 3 months full access or a monthly
+            subscription option, which can be cancelled at any time. See our
+            pricing page for further details. Also included in the annual
+            subscription is Garda vetting on request. See our Garda vetting page
+            for more details. SureSitter does not set the rates of pay for
+            childminders and babysitters. Parents and childminders agree on this
+            together once we have made the introductions.
+          </p>
+          <h3>Are the carers background checked?</h3>
+          <p>
+            Trust is at the heart of our platform. As parents ourselves of small
+            children we understand how important it is to find a trusted
+            childminder or nanny. We do not carry out background checks. We
+            recommend that you check all employment history, references,
+            immigration and visa status so you can be sure that you are making
+            the most informed decision possible. We provide parents with the
+            tools necessary to enable you to find the right caregiver for you
+            and your family. We also carry out ID-verification checks. This
+            provides you with an additional layer of trust. If a sitter has been
+            ID-verified you will see the following badge on their profile. We do
+            this by working with the 3rd party providers used by some of the
+            world’s biggest financial institutions and marketplaces like us.
+          </p>
+          <h3>Who is behind GoNanny?</h3>
+          <p>
+            GoNanny was founded by number of workings parents who struggled to
+            find suitable childcare. The business is backed by a great team of
+            advisors, parents, and childminders whose ultimate goal is to create
+            the best experience possible for parents and carers. Here is some
+            information About Us.
+          </p>
+          <h3>Where does SureSitter operate?</h3>
+          <p>We operate nationwide throughout Ireland.</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FAQParents;
