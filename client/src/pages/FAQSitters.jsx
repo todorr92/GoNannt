e65c20f1b2df5a633c413e79bb9cba68a486d340
unknown <todorovicslobodan10@gmail.com>
2023-07-28 19:38:04 +0100

    added content to FAQ sitter page

--- client/src/pages/FAQSitters.jsx
+++ client/src/pages/FAQSitters.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 const FAQSitters = () => {
-  return <div>FAQSitters</div>;
+  return (
+    <>
+      FAQSitters
+      <div className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6 landing-text-container">
+              <h1 className="landing-header header-color">FAQ's for Sitters</h1>
+              <p className="landing-paragraph text-color">
+                Everything you need to know as a Childminder, Nanny or
+                Babysitter on GoNanny
+              </p>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <img src="src/assets/images/nanny-kids.png" alt="Nanny" />
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <h2>FAQs for Nannies, Childminders, Au Pairs and Babysitters</h2>
+          <h3>Who can sign up to GoNanny?</h3>
+          <p>
+            If you are looking for a babysitting, childminding, nanny or au-pair
+            job in Ireland and are aged over 18 then we’d love you to join our
+            GoNanny community.
+          </p>
+          <h3>Are there any age restrictions?</h3>
+          <p>You must be over 18 to join GoNanny.</p>
+          <h3>How much will it cost me? </h3>
+          <p>Nothing! There is no charge to join GoNanny.</p>
+          <h3>How do I get paid?</h3>
+          <p>
+            Parents will pay you directly and you keep all the money you earn,
+            we do not take a percentage. GoNanny does not set the rate that you
+            are paid. This is agreed directly between the parent and
+            nanny/childminder. We recommend that how and when payments are made
+            are agreed at this stage too.
+          </p>
+          <h3>Who is behind GoNany?</h3>
+          <p>
+            GoNanny was founded by number of workings parents who struggled to
+            find suitable childcare. The business is backed by a great team of
+            advisors, parents, and childminders whose ultimate goal is to create
+            the best experience possible for parents and carers. Here is some
+            information About Us.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FAQSitters;
