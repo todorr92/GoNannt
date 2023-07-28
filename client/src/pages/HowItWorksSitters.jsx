82cf7a8ebcfc12b2863cc975d7d2bd5f9410ab88
unknown <todorovicslobodan10@gmail.com>
2023-07-28 19:39:13 +0100

    added content to how it works for sitters page

--- client/src/pages/HowItWorksSitters.jsx
+++ client/src/pages/HowItWorksSitters.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const HowItWorksSitters = () => {
-  return <div>HowItWorksSitters</div>;
+  return (
+    <>
+      HowItWorksSitters
+      <div className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6 landing-text-container">
+              <h1 className="landing-header header-color">
+                How Does GoNanny Work?
+              </h1>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <img src="src/assets/images/sitter.png" alt="Sitter" />
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <h3>How does GoNanny work for childminders and babysitters?</h3>
+          <p>
+            GoNanny connects parents to trusted sitters, like you! We’re
+            providing an easy, safe and trusted way for you to do what you love
+            and build a great income. Getting set up is easy – as you’d expect,
+            there’ll be checks to make sure you’ll provide a professional
+            service, but we’ve made process simple and quick. Here’s how it
+            works.
+          </p>
+          <h3>1. Create your profile</h3>
+          <p>Your profile is how you’ll be smart-matched to jobs.</p>
+          <h3>2. Apply for jobs</h3>
+          <p>Apply for jobs and respond to direct invites from parents.</p>
+          <h3>3. Do what you love</h3>
+          <p>
+            Meet up and interview, agree a rate and earn money doing what you
+            love.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HowItWorksSitters;
