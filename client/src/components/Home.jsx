a854294d5bea380e874d36541acdcbc0a653b824
unknown <todorovicslobodan10@gmail.com>
2023-07-31 20:33:13 +0100

    added some more styles for Home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
+  const [isActiveParents, setActiveParents] = useState(true);
+  const [isActiveSitters, setActiveSitters] = useState(false);
+  const toggleClassParents = () => {
+    setActiveParents(!isActiveParents);
+    setActiveSitters(false);
+  };
+  const toggleClassSitters = () => {
+    setActiveSitters(!isActiveSitters);
+    setActiveParents(false);
+  };
   return (
     <>
       <div className="container-fluid home-container">
         <div className="container-fluid home">
           <div className="row justify-content-center">
             <div className="col-sm-12 col-md-6 landing-text-container">
-              <h1 className="landing-header header-color">
+              <h1 className="landing-header header-color fw-bold">
                 Find Trusted Childcare in Ireland
               </h1>
               <h3>Founded by Irish parents, for parents.</h3>
@@ -25,116 +37,220 @@ const Home = () => {
         <div className="container-fluid home-works">
           <div className="container pt-5">
             <div className="row text-center">
-              <h2 className="header-color">How GoNanny works</h2>
+              <h2 className="fw-bold">How GoNanny works</h2>
               <p className="text-color">
                 Whether you’re a parent or a childcare professional, we’ve made
                 childcare as easy as 1, 2, 3
               </p>
               <div className="col-sm-12">
-                <button className="button d-inline-block" id="round-parents">
+                <button
+                  className={
+                    isActiveParents
+                      ? "button d-inline-block active"
+                      : "button d-inline-block"
+                  }
+                  onClick={toggleClassParents}
+                  disabled={isActiveParents}
+                  id="round-parents"
+                >
                   For Parents
                 </button>
 
                 <button
-                  className="button d-inline-block active"
+                  className={
+                    isActiveSitters
+                      ? "button d-inline-block active"
+                      : "button d-inline-block"
+                  }
                   id="round-sitters"
+                  onClick={toggleClassSitters}
+                  disabled={isActiveSitters}
                 >
                   For Sitters
                 </button>
               </div>
             </div>
             <div className="row text-center">
-              <div className="col-6">
-                <img src="src/assets/images/parent.png" alt="Parent" />
+              <div className={isActiveParents ? "row" : "row d-none"}>
+                <div className="col-6">
+                  <img
+                    className="img-fluid"
+                    src="src/assets/images/parent.png"
+                    alt="Parent"
+                  />
+                </div>
+                <div className="col-6">
+                  <h3 className="fw-bold mt-5 mb-3">For Parents</h3>
+                  <ul className="parent">
+                    <li className="position-relative">
+                      <div className="number">1</div>
+                      <h5 className="fw-bold">Post a job</h5>
+                      <p>Tell us what you need</p>
+                    </li>
+                    <li className="position-relative">
+                      <div className="number">2</div>
+                      <h5 className="fw-bold">Connect with carers</h5>
+                      <p>
+                        View smart-matched profiles and invite carers to apply
+                      </p>
+                    </li>
+                    <li className="position-relative mb-5">
+                      <div className="number">3</div>
+                      <h5 className="fw-bold">Interview and select</h5>
+                      <p>Choose a carer, agree terms, enjoy the support</p>
+                    </li>
+                    <Link className="button me-5" to="/parents/how-it-works">
+                      Learn More
+                    </Link>
+                    <Link className="button" to="/jobs-board">
+                      Post a job
+                    </Link>
+                  </ul>
+                </div>
               </div>
-              <div className="col-6">
-                <ul className="parent">
-                  <li>
-                    <div>1</div>
-                    <h5>Post a job</h5>
-                    <p>Tell us what you need</p>
-                  </li>
-                  <li>
-                    <div>2</div>
-                    <h5>Connect with carers</h5>
-                    <p>
-                      View smart-matched profiles and invite carers to apply
-                    </p>
-                  </li>
-                  <li>
-                    <div>3</div>
-                    <h5>Interview and select</h5>
-                    <p>Choose a carer, agree terms, enjoy the support</p>
-                  </li>
-                  <button>Learn More</button>
-                  <button>Find Jobs</button>
-                </ul>
-                <ul className="sitter d-none">
-                  <li>
-                    <div>1</div>
-                    <h5>Create your profile</h5>
-                    <p>
-                      Share your personality, your experience and your passion
-                      for childcare.
-                    </p>
-                  </li>
-                  <li>
-                    <div>2</div>
-                    <h5>Search and apply</h5>
-                    <p>
-                      Search and apply for jobs and respond to parent/carer
-                      invites.
-                    </p>
-                  </li>
-                  <li>
-                    <div>3</div>
-                    <h5>Do what you love</h5>
-                    <p>
-                      Interview, agree a rate and earn money doing what you
-                      love.
-                    </p>
-                  </li>
-                  <button>Learn More</button>
-                  <button>Find Jobs</button>
-                </ul>
+              <div
+                className={
+                  isActiveSitters ? "row text-center" : "row text-center d-none"
+                }
+              >
+                <div className="col-6">
+                  <img
+                    className="img-fluid"
+                    src="src/assets/images/nanny-kids.png"
+                    alt="Sitter"
+                  />
+                </div>
+                <div className="col-6">
+                  <h3 className="fw-bold mb-3 mt-5">For Sitters</h3>
+                  <ul className="sitter">
+                    <li className="position-relative">
+                      <div className="number">1</div>
+                      <h5 className="fw-bold">Create your profile</h5>
+                      <p>
+                        Share your personality, your experience and your passion
+                        for childcare.
+                      </p>
+                    </li>
+                    <li className="position-relative">
+                      <div className="number">2</div>
+                      <h5 className="fw-bold">Search and apply</h5>
+                      <p>
+                        Search and apply for jobs and respond to parent/carer
+                        invites.
+                      </p>
+                    </li>
+                    <li className="position-relative mb-5">
+                      <div className="number">3</div>
+                      <h5 className="fw-bold">Do what you love</h5>
+                      <p>
+                        Interview, agree a rate and earn money doing what you
+                        love.
+                      </p>
+                    </li>
+                    <Link className="button me-5" to="/sitters/how-it-works">
+                      Learn More
+                    </Link>
+                    <Link className="button" to="/jobs-board">
+                      Find Jobs
+                    </Link>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="container">
           <div className="row text-center">
-            <h2>Why GoNany?</h2>
-            <p>Sign-up.Select.Sorted.</p>
-            <p>
+            <h2 className="fw-bold mt-5">Why GoNanny?</h2>
+            <p className="fw-bold fst-italic purple">Sign-up.Select.Sorted.</p>
+            <p className="text-wrap">
               We’re parents, we know the stress of finding childcare. That’s why
               we created SureSitter, to make it easy to find trusted local
               childminders, nannies, and au pairs all recommended by parents and
-              carers in your area.{" "}
+              carers in your area.
             </p>
+          </div>
+          <div className="row mt-5 mb-5">
             <div className="col-6">
-              <h4>Supporting Parents</h4>
-              <ul>
-                <li>Find local trusted nannies and childminders</li>
-                <li>Message carers directly</li>
-                <li>Ready to book</li>
-                <li>Easily see if sitter is ID verified and Garda-vetted</li>
+              <h4 className="purple mb-3">Supporting Parents</h4>
+              <ul className="checkbox-list">
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#aea0ff" }}
+                    className="me-3"
+                  />
+                  Find local trusted nannies and childminders
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#aea0ff" }}
+                    className="me-3"
+                  />
+                  Message carers directly
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#aea0ff" }}
+                    className="me-3"
+                  />
+                  Ready to book
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#aea0ff" }}
+                    className="me-3"
+                  />
+                  Easily see if sitter is ID verified and Garda-vetted
+                </li>
               </ul>
             </div>
             <div className="col-6">
-              <h4>Empowering Sitters</h4>
-              <ul>
-                <li>Create a profile to share your expertise</li>
-                <li>Find local childcare jobs</li>
+              <h4 className="text-color mb-3">Empowering Sitters</h4>
+              <ul className="checkbox-list">
                 <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#4c79b9" }}
+                    className="me-3"
+                  />
+                  Create a profile to share your expertise
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#4c79b9" }}
+                    className="me-3"
+                  />
+                  Find local childcare jobs
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#4c79b9" }}
+                    className="me-3"
+                  />
                   Get smart-matched based on your location and the jobs you want
                 </li>
-                <li>Receive invites from parents</li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    style={{ color: "#4c79b9" }}
+                    className="me-3"
+                  />
+                  Receive invites from parents
+                </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className="container">
-          <h2>What some of our happy Parents & Sitters are saying</h2>
+        <div className="container mb-5">
+          <h2 className="fw-bold text-center mb-5">
+            What some of our happy Parents & Sitters are saying
+          </h2>
           <div className="row justify-content-center">
             <div className="col-4">
               <article>
@@ -143,7 +259,8 @@ const Home = () => {
                     “I love SureSitter. We recently moved to Ireland from abroad
                     and we have no family here. We put up a job looking for an
                     occasional babysitter and we found a fantastic babysitter
-                    who lives a few streets away from us”
+                    who lives a few streets away from us. Some more text Some
+                    more text Some more text Some more text”
                   </p>
                   <i>Laura</i>
                 </div>
@@ -169,7 +286,9 @@ const Home = () => {
                   <p>
                     “Within a few hours of posting my job I had responses from 5
                     excellent babysitters. Would definitely use again. Some more
-                    more more more more more more text. Another line of text”
+                    more more more more more more text. Another line of text.
+                    Within a few hours of posting my job I had responses from 5
+                    excellent babysitters. ”
                   </p>
                   <i>Anna</i>
                 </div>
@@ -177,13 +296,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="container">
-          <h2>Why We're Different</h2>
+        <div className="container mb-5">
+          <h2 className="fw-bold text-center mb-5">Why We're Different</h2>
           <div className="row">
-            <div className="col-6">
-              <img src="src/assets/images/people.png" alt="People" />
+            <div className="col-6 mb-5">
+              <img
+                src="src/assets/images/people.png"
+                alt="People"
+                className="img-fluid people-image"
+              />
             </div>
-            <div className="col-6">
+            <div className="col-6 mt-5">
               <h4>Created for Parents by Parents</h4>
               <p>
                 As parents, we understand how difficult it is to find trusted
@@ -194,7 +317,9 @@ const Home = () => {
                 you access to trusted providers all over Ireland, so that you
                 can feel confident booking qualified, local carers in a flash.
               </p>
-              <button>Learn More About Us</button>
+              <Link className="button" to="/about">
+                Learn More About Us
+              </Link>
             </div>
           </div>
         </div>
