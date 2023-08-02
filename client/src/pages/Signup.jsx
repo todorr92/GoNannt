bfd665e13fd1e1b1915fc3192c0ab8b3322f3b69
unknown <todorovicslobodan10@gmail.com>
2023-08-02 14:24:56 +0100

    added sign up content and styles

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSignup } from "../hooks/useSignup";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -14,45 +15,71 @@ const Signup = () => {
   };
   return (
     <>
-      <div className="container-fluid" id="join-us">
+      <div className="container-fluid light-blue-background" id="join-us">
         <div className="container" id="join-us-container">
-          <form id="register" onSubmit={handleSubmit}>
-            <h1 className="h3 mb-3">Please signup</h1>
-            <div className="form-floating">
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-              />
-              <label htmlFor="name">Full Name</label>
+          <div className="row justify-content-center">
+            <div className="col-6">
+              <Link to="/">
+                <img
+                  src="src/assets/images/cover.png"
+                  alt="logo"
+                  className="img-fluid"
+                />
+              </Link>
             </div>
-            <div className="form-floating">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <label htmlFor="email">Email</label>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-12 w-50 shadow bg-light">
+              <form id="login" onSubmit={handleSubmit} className="text-center">
+                <h1 className="h3 mb-4 fw-bold pt-4">Sign up to GoNanny</h1>
+
+                <div className="form-floating w-75 m-auto">
+                  <input
+                    type="name"
+                    className="form-control mb-3"
+                    id="name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                  />
+                  <label htmlFor="name">Full Name</label>
+                </div>
+                <div className="form-floating w-75 m-auto mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                  <label htmlFor="email">Email</label>
+                </div>
+                <div className="form-floating w-75 m-auto">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <button
+                  className="button mt-5 mb-5"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  Sign Up
+                </button>
+                {error && <div className="error">{error}</div>}
+                <p>
+                  Already have an account?{" "}
+                  <Link className="purple singup-link" to="/login">
+                    Login
+                  </Link>
+                </p>
+              </form>
             </div>
-            <div className="form-floating">
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              <label htmlFor="password">Password</label>
-            </div>
-            <button className="btn btn-primary" disabled={isLoading}>
-              Register
-            </button>
-            {error && <div className="error">{error}</div>}
-          </form>
+          </div>
         </div>
       </div>
     </>
